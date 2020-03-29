c15e793bf1ce7c5993d8d7f4b1f37774765f424e
omidmirzajani <omidmirzajani82@gmail.com>
2020-03-29 09:41:37 +0430

    A9 Added

---
+++ A9/Code/A9/A9/Q4ParallelProcessing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using TestCommon;
+
+namespace A9
+{
+    public class Q4ParallelProcessing : Processor
+    {
+        public Q4ParallelProcessing(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[], Tuple<long, long>[]>)Solve);
+
+        public Tuple<long, long>[] Solve(long threadCount, long[] jobDuration)
+        {
+            Tuple<long, long>[] a = new Tuple<long, long>[threadCount];
+            for (int i = 0; i < threadCount; i++)
+                a[i] = new Tuple<long, long>(i, 0);
+            return a;
+        }
+    }
+}
